c4b4e10767bb448f9d8f111383a87959d0f00ba5
unknown <mailsonmartins@gmail.com>
2020-09-11 12:59:03 -0300

    Criado o arquivo css home e iniciado a pagina index do sistema

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,92 @@
 @extends('layout.app')
 
 @section('body')
-    <h1>Ola</h1>
+
+<div class="row">
+
+    <div class="card border-primary col-sm-6">
+        <div class="card-body text-primary">
+          <h3 class="card-title">Bem vindo ao sistema de cadastro de produtos!</h3>
+          <p class="card-text">Escolha uma das opções ao lado:</p>
+        </div>
+    </div>
+    <div class="col-sm-6">
+        
+        <div class="card-group">
+            <div class="col-sm-12 title-card-group">
+                <h4>Produtos</h4>
+            </div>
+            <div class="col-sm-6 cardPadding mb-3">
+                <div class="card text-white bg-success" >
+                    <div class="card-body">
+                        <h5 class="card-title">Cadastrar</h5>
+                        <p class="card-text">Cadastrar um produto</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 cardPadding mb-3">
+                <div class="card text-white bg-warning" >
+                    <div class="card-body">
+                        <h5 class="card-title">Editar</h5>
+                        <p class="card-text">Editar um produto</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 cardPadding">
+                <div class="card text-white bg-primary" >
+                    <div class="card-body">
+                        <h5 class="card-title">Buscar</h5>
+                        <p class="card-text">Pesquisar um produto</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 cardPadding">
+                <div class="card text-white bg-danger" >
+                    <div class="card-body">
+                        <h5 class="card-title">Deletar</h5>
+                        <p class="card-text">Deletar um produto</p>
+                    </div>
+                </div>
+            </div>
+    
+            <div class="col-sm-12 title-card-group mt-3">
+                <h4>Categorias</h4>
+            </div>
+            <div class="col-sm-6 cardPadding mb-3">
+                <div class="card text-white bg-success" >
+                    <div class="card-body">
+                        <h5 class="card-title">Cadastrar</h5>
+                        <p class="card-text">Cadastrar uma categoria</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 cardPadding mb-3">
+                <div class="card text-white bg-warning" >
+                    <div class="card-body">
+                        <h5 class="card-title">Editar</h5>
+                        <p class="card-text">Editar uma categoria</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 cardPadding">
+                <div class="card text-white bg-primary" >
+                    <div class="card-body">
+                        <h5 class="card-title">Buscar</h5>
+                        <p class="card-text">Pesquisar uma categoria</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 cardPadding">
+                <div class="card text-white bg-danger" >
+                    <div class="card-body">
+                        <h5 class="card-title">Deletar</h5>
+                        <p class="card-text">Deletar uma categoria</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+</div>
+
 @endsection
--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -1,10 +1,18 @@
 <html>
     <head>
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/home.css') }}">
         <title>Cadastro de Produtos</title>
         <meta name="csrf-token" content="{{ csrf_token() }}">
     </head>
     <body>
+        <nav class="navbar navbar-dark bg-primary">
+            <a class="navbar-brand">Sistema de cadastro de produtos</a>
+            <form class="form-inline">
+                <input class="form-control mr-sm-2" type="search" placeholder="Procurar" aria-label="Search">
+                <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Buscar</button>
+            </form>
+        </nav>
         <div class="container">
             <main role="main">
                 @hasSection('body')
