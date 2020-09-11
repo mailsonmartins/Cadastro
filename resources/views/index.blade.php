d77f811b789d4c61bed0ac2e39e4e01cb077c910
unknown <mailsonmartins@gmail.com>
2020-09-11 14:06:38 -0300

    Criado o arquivo footer.css e adicionado o rodape, alem de melhoria no layout da home

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,81 +10,91 @@
           <p class="card-text">Escolha uma das opções ao lado:</p>
         </div>
     </div>
+
     <div class="col-sm-6">
         
         <div class="card-group">
-            <div class="col-sm-12 title-card-group">
+            <div class="col-sm-12 title-card-group bg-primary mb-3">
                 <h4>Produtos</h4>
             </div>
-            <div class="col-sm-6 cardPadding mb-3">
+            <div class="col-sm-6 cardPaddingRight mb-3">
                 <div class="card text-white bg-success" >
                     <div class="card-body">
+                        <h1><i class="fa fa-plus"></i></h1>
                         <h5 class="card-title">Cadastrar</h5>
                         <p class="card-text">Cadastrar um produto</p>
                     </div>
                 </div>
             </div>
-            <div class="col-sm-6 cardPadding mb-3">
+            <div class="col-sm-6 cardPaddingLeft mb-3">
                 <div class="card text-white bg-warning" >
                     <div class="card-body">
+                        <h1><i class="fa fa-pencil"></i></h1>
                         <h5 class="card-title">Editar</h5>
                         <p class="card-text">Editar um produto</p>
                     </div>
                 </div>
             </div>
-            <div class="col-sm-6 cardPadding">
-                <div class="card text-white bg-primary" >
+            <div class="col-sm-6 cardPaddingRight mb-3">
+                <div class="card text-white bg-dark" >
                     <div class="card-body">
+                        <h1><i class="fa fa-search"></i></h1>
                         <h5 class="card-title">Buscar</h5>
                         <p class="card-text">Pesquisar um produto</p>
                     </div>
                 </div>
             </div>
-            <div class="col-sm-6 cardPadding">
+            <div class="col-sm-6 cardPaddingLeft mb-3">
                 <div class="card text-white bg-danger" >
                     <div class="card-body">
+                        <h1><i class="fa fa-trash"></i></h1>
                         <h5 class="card-title">Deletar</h5>
                         <p class="card-text">Deletar um produto</p>
                     </div>
                 </div>
             </div>
     
-            <div class="col-sm-12 title-card-group mt-3">
+            <div class="col-sm-12 title-card-group bg-primary mb-3 mt-3">
                 <h4>Categorias</h4>
             </div>
-            <div class="col-sm-6 cardPadding mb-3">
+            <div class="col-sm-6 cardPaddingRight mb-3">
                 <div class="card text-white bg-success" >
                     <div class="card-body">
+                        <h1><i class="fa fa-plus"></i></h1>
                         <h5 class="card-title">Cadastrar</h5>
                         <p class="card-text">Cadastrar uma categoria</p>
                     </div>
                 </div>
             </div>
-            <div class="col-sm-6 cardPadding mb-3">
+            <div class="col-sm-6 cardPaddingLeft mb-3">
                 <div class="card text-white bg-warning" >
                     <div class="card-body">
+                        <h1><i class="fa fa-pencil"></i></h1>
                         <h5 class="card-title">Editar</h5>
                         <p class="card-text">Editar uma categoria</p>
                     </div>
                 </div>
             </div>
-            <div class="col-sm-6 cardPadding">
-                <div class="card text-white bg-primary" >
+            <div class="col-sm-6 cardPaddingRight">
+                <div class="card text-white bg-dark" >
                     <div class="card-body">
+                        <h1><i class="fa fa-search"></i></h1>
                         <h5 class="card-title">Buscar</h5>
                         <p class="card-text">Pesquisar uma categoria</p>
                     </div>
                 </div>
             </div>
-            <div class="col-sm-6 cardPadding">
+            <div class="col-sm-6 cardPaddingLeft">
                 <div class="card text-white bg-danger" >
                     <div class="card-body">
+                        <h1><i class="fa fa-trash"></i></h1>
                         <h5 class="card-title">Deletar</h5>
                         <p class="card-text">Deletar uma categoria</p>
                     </div>
                 </div>
             </div>
         </div>
+
     </div>
     
 </div>
--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -2,6 +2,8 @@
     <head>
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
         <link rel="stylesheet" href="{{ asset('css/home.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/footer.css') }}">
+        <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
         <title>Cadastro de Produtos</title>
         <meta name="csrf-token" content="{{ csrf_token() }}">
     </head>
@@ -13,6 +15,7 @@
                 <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Buscar</button>
             </form>
         </nav>
+        
         <div class="container">
             <main role="main">
                 @hasSection('body')
@@ -20,6 +23,51 @@
                 @endif
             </main>
         </div>
+
+        <footer id="myFooter">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-3">
+                        <h2 class="logo"><a href=""> LOGO </a></h2>
+                    </div>
+                    <div class="col-sm-2">
+                        <h5>Inicio</h5>
+                        <ul>
+                            <li><a href="">Home</a></li>
+                            
+                        </ul>
+                    </div>
+                    <div class="col-sm-2">
+                        <h5>Sobre-nós</h5>
+                        <ul>
+                            <li><a href="">Sobre a Empresa</a></li>
+                            <li><a href="">Contato</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-sm-2">
+                        <h5>Suporte</h5>
+                        <ul>
+                            <li><a href="">FAQ</a></li>
+                            <li><a href="">Telefones</a></li>
+                            <li><a href="">Chat</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-sm-3">
+                        <div class="social-networks">
+                            <a href="" class="twitter"><i class="fa fa-twitter"></i></a>
+                            <a href="" class="facebook"><i class="fa fa-facebook"></i></a>
+                            <a href="" class="instagram"><i class="fa fa-instagram"></i></a>
+                        </div>
+                        <a href="">
+                            <button type="button" class="btn btn-default">Contato</button>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="footer-copyright">
+                <p>© 2020 Copyright</p>
+            </div>
+        </footer>
         <script src="{{ asset('js/app.js') }}" type="text/javascript"></script>
     </body>
 </html>
